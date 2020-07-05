d51a3f9f00cdc383ca5fb3c534305b86c3eea693
Duc Nguyen <minhduc140583@gmail.com>
2020-07-05 13:31:02 +0700

    pure function

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,25 @@ export interface TokenService {
   verifyToken(token: string, secret: string): Promise<any>;
 }
 
-export class jwt {
-  static generateToken(payload: any, secret: string, expiresIn: number): Promise<string> {
-    return new Promise<string>(((resolve, reject) => {
-      sign(payload, secret, {expiresIn}, (err, token: string) => err ? reject(err) : resolve(token));
-    }));
-  }
+export function generateToken(payload: any, secret: string, expiresIn: number): Promise<string> {
+  return new Promise<string>(((resolve, reject) => {
+    sign(payload, secret, {expiresIn}, (err, token: string) => err ? reject(err) : resolve(token));
+  }));
+}
 
-  static verifyToken(token: string, secret: string): Promise<any> {
-    return new Promise<any>(((resolve, reject) => {
-      verify(token, secret, (err, payload: any) => err ? reject(err) : resolve(payload));
-    }));
-  }
+export function verifyToken(token: string, secret: string): Promise<any> {
+  return new Promise<any>(((resolve, reject) => {
+    verify(token, secret, (err, payload: any) => err ? reject(err) : resolve(payload));
+  }));
 }
 
 export class DefaultTokenService implements TokenService {
   generateToken(payload: any, secret: string, expiresIn: number): Promise<string> {
-    return jwt.generateToken(payload, secret, expiresIn);
+    return generateToken(payload, secret, expiresIn);
   }
 
   verifyToken(token: string, secret: string): Promise<any> {
-    return jwt.verifyToken(token, secret).then(v => v).catch(err => {
+    return verifyToken(token, secret).then(v => v).catch(err => {
       if (err instanceof TokenExpiredError) {
         err['errorType'] = 'TokenExpired';
       }
