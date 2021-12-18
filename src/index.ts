b11600b60e9f2d1cabd74b2ed2d6e8b0939b0efd
Duc Nguyen <minhduc140583@gmail.com>
2021-12-18 12:20:56 +0700

    change config to strict

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
-import {sign, TokenExpiredError, verify} from 'jsonwebtoken';
+import { JsonWebTokenError, JwtPayload, sign, TokenExpiredError, verify as ver, VerifyErrors } from 'jsonwebtoken';
 
-export interface TokenService {
-  generateToken(payload: any, secret: string, expiresIn: number): Promise<string>;
-  verifyToken(token: string, secret: string): Promise<any>;
-}
-
-export function generateToken(payload: any, secret: string, expiresIn: number): Promise<string> {
-  return new Promise<string>(((resolve, reject) => {
-    sign(payload, secret, {expiresIn}, (err, token: string) => err ? reject(err) : resolve(token));
+export type Payload = JwtPayload | undefined;
+export function generate(payload: any, secret: string, expiresIn: number): Promise<string | undefined> {
+  return new Promise<string | undefined>(((resolve, reject) => {
+    sign(payload, secret, { expiresIn }, (err, token?: string) => err ? reject(err) : resolve(token));
   }));
 }
-
-export function verifyToken(token: string, secret: string): Promise<any> {
-  return new Promise<any>(((resolve, reject) => {
-    verify(token, secret, (err, payload: any) => err ? reject(err) : resolve(payload));
+export function verify(token: string, secret: string): Promise<Payload> {
+  return new Promise<Payload>(((resolve, reject) => {
+    ver(token, secret, (err: VerifyErrors | null, payload: Payload) => err ? reject(err) : resolve(payload));
   }));
 }
-
-export class DefaultTokenService implements TokenService {
-  generateToken(payload: any, secret: string, expiresIn: number): Promise<string> {
-    return generateToken(payload, secret, expiresIn);
+export class TokenService {
+  constructor() {
+    this.generate = this.generate.bind(this);
+    this.verify = this.verify.bind(this);
   }
-
-  verifyToken(token: string, secret: string): Promise<any> {
-    return verifyToken(token, secret).then(v => v).catch(err => {
-      if (err instanceof TokenExpiredError) {
-        err['errorType'] = 'TokenExpired';
-      }
-      throw err;
-    });
+  generate(payload: any, secret: string, expiresIn: number): Promise<string | undefined> {
+    return generate(payload, secret, expiresIn);
   }
+  verify(token: string, secret: string): Promise<Payload> {
+    return verify(token, secret);
+  }
+}
+export function buildJwtError(err: any): { status: number, body: string } {
+  if (err instanceof TokenExpiredError) {
+    return { status: 401, body: 'the token is expired' };
+  } else if (err instanceof JsonWebTokenError) {
+    return { status: 401, body: 'invalid token' };
+  } else {
+    return { status: 500, body: 'Internal Server Error ' + toString(err) };
+  }
+}
+export function toString(err: any): string {
+  return (typeof err === 'string' ? err : JSON.stringify(err));
 }
